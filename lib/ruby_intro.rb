5c393bd603e347ff7134ab91a0c4322c37936d0c
Andrew Bissada <andrew.bissada@gmail.com>
2015-09-03 20:20:06 +0000

    Finished Part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,20 +1,61 @@
+#Author: Andrew Bissada
+#Class: CSCI-602
+#HW: Ruby-Assignment 1
+##
 # When done, submit this entire file to the autograder.
 
-# Part 1
+# PART 1
 
-def sum arr
-  # YOUR CODE HERE
+# Takes an array of integers as an argument and returns the sum of its elements. For an empty array it should return zero.
+def sum (arr)
+    sum = 0
+    x=arr.each do |n| 
+        # increment some SUM
+        sum = sum + n
+    end
+    
+    if arr.empty?
+        sum = 0
+    end
+    
+    return sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+
+# Takes an array of integers as an argument and returns the sum of its two largest elements. 
+# For an empty array it should return zero. For an array with just one element, it should return that element.
+def max_2_sum (arr)
+   
+   if arr.empty?
+       sum = 0
+   elsif arr.length == 1
+       sum = arr[0]
+   else
+       arr.sort!.reverse!
+       sum = arr[0]+arr[1]
+   end
+   return sum
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+
+# Takes an array of integers and an additional integer, n, as arguments and returns true if any two elements in the array of integers sum to n. 
+# An empty array should sum to zero by definition
+def sum_to_n? (arr,n)
+    
+    a2 = arr.combination(2).to_a
+        answer = false #this is not static so value will be changed from forEach loop
+    a2.each do |sub_array| 
+        
+        #sum = 0
+        if sub_array[0] + sub_array[1] == n
+            answer = true
+        end
+    end
+    return answer
 end
 
-# Part 2
+##############
+# PART 2
 
 def hello(name)
   # YOUR CODE HERE
@@ -28,7 +69,9 @@ def binary_multiple_of_4? s
   # YOUR CODE HERE
 end
 
-# Part 3
+
+###############
+# PART 3
 
 class BookInStock
 # YOUR CODE HERE
